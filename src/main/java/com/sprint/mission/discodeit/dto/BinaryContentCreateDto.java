1958c365bc097457c77519e4bcac40f1e871c11c
Seongmin Kang <seongmin0244@hanyang.ac.kr>
2026-03-17 10:00:18 +0900

    feat: BinaryContentService CRUD 기능 및 DTO 구현

---
+++ src/main/java/com/sprint/mission/discodeit/dto/BinaryContentCreateDto.java
@@ -0,0 +1,6 @@
+package com.sprint.mission.discodeit.dto;
+
+public record BinaryContentCreateDto(
+        byte[] bytes
+) {
+}
--- src/main/java/com/sprint/mission/discodeit/repository/BinaryContentRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/BinaryContentRepository.java
@@ -2,10 +2,11 @@ package com.sprint.mission.discodeit.repository;
 
 import com.sprint.mission.discodeit.entity.BinaryContent;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface BinaryContentRepository {
     BinaryContent save(BinaryContent binaryContent);
-    BinaryContent findById(UUID id);
+    Optional<BinaryContent> findById(UUID id);
     void delete(UUID id);
 }
