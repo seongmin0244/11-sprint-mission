e20ad3c9b41254370965a69f2af25aade6ce1ad3
Seongmin Kang <seongmin0244@hanyang.ac.kr>
2026-03-24 17:28:37 +0900

    feat: 전역 예외 처리(Global Exception Handling) 구현 및 ErrorResponse 레코드 생성

---
+++ src/main/java/com/sprint/mission/discodeit/dto/ErrorResponse.java
@@ -0,0 +1,11 @@
+package com.sprint.mission.discodeit.dto;
+
+import java.time.Instant;
+
+public record ErrorResponse(
+        Instant timeStamp,
+        int status,
+        String error,
+        String message
+) {
+}
---
+++ src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.sprint.mission.discodeit.exception;
+
+import com.sprint.mission.discodeit.dto.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.Instant;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleBadRequest(IllegalArgumentException e) {
+        ErrorResponse response = new ErrorResponse(
+                Instant.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                "Bad Request",
+                e.getMessage()
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+}
