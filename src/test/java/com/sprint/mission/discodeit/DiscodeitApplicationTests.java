4f081d87a9a6f19133e93edb482700f591400d9d
Seongmin Kang <seongmin0244@hanyang.ac.kr>
2026-05-21 10:13:46 +0900

    test(DiscodeitApplicationTests): DB 미연결로 인한 contextLoads 테스트 비활성화

--- src/test/java/com/sprint/mission/discodeit/DiscodeitApplicationTests.java
+++ src/test/java/com/sprint/mission/discodeit/DiscodeitApplicationTests.java
@@ -1,13 +1,15 @@
 package com.sprint.mission.discodeit;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 class DiscodeitApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
