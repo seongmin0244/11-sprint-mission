f3916fd062d7cbb91cf446f395933461c58f9569
Seongmin Kang <seongmin0244@hanyang.ac.kr>
2026-07-10 17:20:44 +0900

    refactor: 데드 코드 제거

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicMessageService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicMessageService.java
@@ -26,7 +26,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
-import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
