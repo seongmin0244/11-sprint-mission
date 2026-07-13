b479bcde368c856b4bfc1dc7946608258c715571
Seongmin Kang <seongmin0244@hanyang.ac.kr>
2026-07-13 21:27:26 +0900

    feat: 캐시 적용 - Caffeine 캐시 환경 구성

--- src/main/java/com/sprint/mission/discodeit/config/AppConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.sprint.mission.discodeit.config;
 
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -7,6 +8,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @Configuration
 @EnableScheduling
 @EnableRetry
+@EnableCaching
 public class AppConfig {
 
 }
