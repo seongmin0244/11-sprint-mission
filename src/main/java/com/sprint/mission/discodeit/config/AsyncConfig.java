2de34ae9f0951f5e3e67a42f0d15896d9a049974
Seongmin Kang <seongmin0244@hanyang.ac.kr>
2026-07-21 20:25:23 +0900

    refactor: AsyncConfig의 로깅(MDC) 관심사와 보안(SecurityContext) 관심사 분리

--- src/main/java/com/sprint/mission/discodeit/config/AsyncConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/AsyncConfig.java
@@ -3,16 +3,12 @@ package com.sprint.mission.discodeit.config;
 import com.sprint.mission.discodeit.config.decorator.MDCTaskDecorator;
 import com.sprint.mission.discodeit.config.decorator.SecurityContextTaskDecorator;
 import java.util.List;
-import java.util.Map;
-import org.slf4j.MDC;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.core.task.support.CompositeTaskDecorator;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 
 @Configuration
 @EnableAsync
